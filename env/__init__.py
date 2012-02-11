e273c713d1f56a272ec27a099f28e63cb9e82b88
Tomek Czyż <tomekczyz@gmail.com>
2012-02-11 16:44:35 +0100

    add logging, add comments, user new settings options

--- env/__init__.py
+++ env/__init__.py
@@ -7,10 +7,19 @@ from .settings import DJANGO_ENV_UPDATE_REQUIREMENTS
 from .settings import DJANGO_ENV_CREATE_AUTOUPDATE
 from .settings import DJANGO_ENV_CREATE_SITEPACKAGES
 
-PROJECT_PATH = os.path.abspath('.')
+from logging import getLogger
+import logging
 
-       # os.path.dirname(os.path.normpath(
-       #             os.sys.modules[settings].__file__))
+log = getLogger('django-env')
+log.setLevel(logging.DEBUG)
+
+stdout_formatter = logging.Formatter(" * [%(levelname)-7s] - %(message)s")
+stdout_handler = logging.StreamHandler()
+stdout_handler.setFormatter(stdout_formatter)
+
+log.addHandler(stdout_handler)
+log.debug('-- DJANGO-ENV START --')
+log.debug('settings: %s' % settings)
 
 VERSION = "0.2"
 
@@ -26,54 +35,75 @@ if not os.path.isabs(path):
 
 # Try to load projects settings
 
-def setup(autocreate=False):
-    global path
-    if not os.path.isdir(path):
-        print ' * Path (%s) not exists.' % path
-        if autocreate:
-            create()
-        else:
-            print ' * NOT autocreating environment'
-            return False
-    return activate()
+try:
+    settings = __import__('settings')
+except Exception as e:
+    log.error("Could not load project settings (project dir: %s)" %
+              (PROJECT_PATH))
 
+log.debug('django-env: %s' % path)
+log.debug('settings: %s' % settings)
+
+
+# DEFINE FUNCTIONS
 
 def activate():
+    '''
+    Activate environment - call env/bin/activate_this.py.
+    '''
     global path
-    print ' * SET UP DJANGO ENV TO %s' % path
     try:
+        if not os.path.isdir(path):
+            raise Exception('Path with environment (%s) not exists' % path)
         activate_this = os.path.join(path, 'bin', 'activate_this.py')
         execfile(activate_this, dict(__file__=activate_this))
-    except:
-        print 'Error while activating environment %s' % path
+        log.info('Django env set to: %s' % path)
+    except Exception as e:
+        #log.exception("%s" % e)
+        log.error("%s" % e)
+        log.error('ERROR while activating environment %s' % path)
         return
 
 
 def create():
+    '''
+    If path with environment not exists, create new one.
+    If path exists, you can force recreating by passing `--force`.
+    If *DJANGO_ENV_CREATE_AUTOUPDATE* is set to *True*, call `env_update`.
+    '''
     global path
     import virtualenv as ve
     if os.path.isdir(path):
-        print 'Path with environment (%s) exist. If you want to new run '\
-              ' `env_delete` first, or use --force' % path
+        log.error('Path with environment (%s) exist. If you want to new run '\
+              ' `env_delete` first, or use --force' % path)
         return
-    print ' * Creating NEW environment in %s' % path
+    log.info(' * Creating NEW environment in %s' % path)
     ve.create_environment(path,
                           site_packages=True,
                           clear=True,
                          )
-    if getattr(settings, 'DJANGO_ENV_AUTOUPDATE', DJANGO_ENV_AUTOUPDATE):
+    if getattr(settings, 'DJANGO_ENV_CREATE_AUTOUPDATE',
+               DJANGO_ENV_CREATE_AUTOUPDATE):
+        log.debug('autoupdate: on')
         update()
+    else:
+        log.debug('autoupdate: off')
 
 
 def update(upgrade=False):
+    '''
+    Install packages in virtual environment (call pip install -r
+    DJANGO_ENV_UPDATE_REQUIREMENTS)
+    '''
     global path, PROJECT_PATH
     if not os.path.isdir(path):
-        print 'Path with environment (%s) not exist. Run `env_create` first'
+        log.error('Path with environment (%s) not exist. Run `env_create` '
+                  'first')
         return
-    print ' * Updating environment in %s' % path
     req = os.path.join(PROJECT_PATH,
-                       getattr(settings, 'DJANGO_ENV_REQUIREMENTS',
-                                          DJANGO_ENV_REQUIREMENTS))
+                       getattr(settings, 'DJANGO_ENV_UPDATE_REQUIREMENTS',
+                                          DJANGO_ENV_UPDATE_REQUIREMENTS))
+    log.info('Updating environment in: %s' % path)
     import virtualenv as ve
     print ' * Updating pip packages from %s' % req
     from pip.baseparser import parser
@@ -87,10 +117,13 @@ def update(upgrade=False):
 
 
 def delete():
+    '''
+    If virtual environment's directory exists, delete it.
+    '''
     global path
-    print ' * Deleting environment in %s' % path
+    log.info('Deleting environment in %s' % path)
     if not os.path.isdir(path):
-        print 'Path with environment (%s) not exist. Nothing to do.'
+        log.info('Path with environment (%s) not exist. Nothing to do.' % path)
         return
     import shutil
     shutil.rmtree(path)
